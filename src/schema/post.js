9e103dea8e8522736e6c713e8ec2f4a0b0f46201
Aman Prasad <amanprasad.9576@gmail.com>
2024-11-28 22:09:50 +0530

    error solve of schema

--- src/schema/post.js
+++ src/schema/post.js
@@ -15,13 +15,6 @@ const postSchema = new mongoose.Schema({
     type: mongoose.Schema.Types.ObjectId,
     ref: "User"
 },
-  comment :{
-    type:String,
-    like:may ,
-  },
-  like :{
-    like: may ,
-  },
   
 
 },{ timestamps :true});
